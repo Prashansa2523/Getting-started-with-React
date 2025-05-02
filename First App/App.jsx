273778ef8b3e474d5928ecaac53bbe9ef2df6c0a
Prashansa2523 <psnk2523@gmail.com>
2025-05-02 19:46:02 +0530

    test

--- First App/App.jsx
+++ First App/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, Alert, SafeAreaView, TextInput, Animated } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+//testing github
 const Stack = createNativeStackNavigator();
 
 // HomeScreen component
